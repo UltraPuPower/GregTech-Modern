4752d33f087f5edc7d14414c8b5e288af95e1207
kross <135918757+krossgg@users.noreply.github.com>
2024-11-12 22:03:41 -0500

    Fix NoClassFoundException when EMI was not installed (#2325)

--- src/main/java/com/gregtechceu/gtceu/api/recipe/ui/GTRecipeTypeUI.java
+++ src/main/java/com/gregtechceu/gtceu/api/recipe/ui/GTRecipeTypeUI.java
@@ -258,7 +258,8 @@ public class GTRecipeTypeUI {
                                     } else if (LDLib.isEmiLoaded()) {
                                         recipeType.getRecipesByCategory().keySet()
                                                 .forEach(e -> EmiApi
-                                                        .displayRecipeCategory(GTRecipeEMICategory.getCategoryFor(e)));
+                                                        .displayRecipeCategory(
+                                                                GTRecipeEMICategory.CATEGORIES.apply(e)));
                                     }
                                 }
                             }).setHoverTooltips("gtceu.recipe_type.show_recipes"));
