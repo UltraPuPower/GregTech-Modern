a3243add1a7335c668d6e2d7a5929bb938d49adc
Echoloquate <104210539+echoloquate@users.noreply.github.com>
2024-11-17 11:26:10 +0200

    Feature/fisher buff (#2359)

--- src/main/java/com/gregtechceu/gtceu/common/machine/electric/FisherMachine.java
+++ src/main/java/com/gregtechceu/gtceu/common/machine/electric/FisherMachine.java
@@ -9,6 +9,7 @@ import com.gregtechceu.gtceu.api.gui.WidgetUtils;
 import com.gregtechceu.gtceu.api.gui.editor.EditableMachineUI;
 import com.gregtechceu.gtceu.api.gui.editor.EditableUI;
 import com.gregtechceu.gtceu.api.gui.widget.SlotWidget;
+import com.gregtechceu.gtceu.api.gui.widget.ToggleButtonWidget;
 import com.gregtechceu.gtceu.api.item.tool.GTToolType;
 import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
 import com.gregtechceu.gtceu.api.machine.TickableSubscription;
@@ -22,6 +23,7 @@ import com.gregtechceu.gtceu.config.ConfigHolder;
 import com.gregtechceu.gtceu.data.lang.LangHandler;
 import com.gregtechceu.gtceu.data.tag.GTDataComponents;
 
+import com.lowdragmc.lowdraglib.gui.texture.ItemStackTexture;
 import com.lowdragmc.lowdraglib.gui.texture.ResourceTexture;
 import com.lowdragmc.lowdraglib.gui.widget.*;
 import com.lowdragmc.lowdraglib.side.item.ItemTransferHelper;
@@ -132,10 +134,14 @@ public class FisherMachine extends TieredEnergyMachine
     private static final ItemStack fishingRod = new ItemStack(Items.FISHING_ROD);
     private boolean hasWater = false;
 
+    @Getter
+    @Setter
+    protected boolean junkEnabled = true;
+
     public FisherMachine(IMachineBlockEntity holder, int tier, Object... ignoredArgs) {
         super(holder, tier);
         this.inventorySize = (tier + 1) * (tier + 1);
-        this.maxProgress = 1000 - tier * 200;
+        this.maxProgress = calcMaxProgress(tier);
         this.energyPerTick = GTValues.V[tier - 1];
         this.cache = createCacheItemHandler();
         this.baitHandler = createBaitItemHandler();
@@ -217,6 +223,10 @@ public class FisherMachine extends TieredEnergyMachine
         clearInventory(cache.storage);
     }
 
+    public static int calcMaxProgress(int tier) {
+        return (int) (800.0 - 170 * ((double) tier - 1.0) + (((double) Math.max(0, tier - 4) / 0.012)));
+    }
+
     //////////////////////////////////////
     // ********* Logic **********//
     //////////////////////////////////////
@@ -257,6 +267,10 @@ public class FisherMachine extends TieredEnergyMachine
 
             LootTable lootTable = getLevel().registryAccess().registry(Registries.LOOT_TABLE).get()
                     .get(BuiltInLootTables.FISHING);
+            if (!this.junkEnabled) {
+                lootTable = getLevel().registryAccess().registry(Registries.LOOT_TABLE).get()
+                        .get(BuiltInLootTables.FISHING_FISH);
+            }
 
             FishingHook simulatedHook = new FishingHook(EntityType.FISHING_BOBBER, getLevel()) {
 
@@ -279,8 +293,10 @@ public class FisherMachine extends TieredEnergyMachine
             for (ItemStack itemStack : generatedLoot)
                 useBait |= tryFillCache(itemStack);
 
-            if (useBait)
+            if (useBait && junkEnabled)
                 this.baitHandler.storage.extractItem(0, 1, false);
+            else if (useBait)
+                this.baitHandler.storage.extractItem(0, 2, false);
             updateFishingUpdateSubscription();
             progress = -1;
         }
@@ -401,6 +417,7 @@ public class FisherMachine extends TieredEnergyMachine
                     createTemplate(inventorySize).setupUI(template, fisherMachine);
                     createEnergyBar().setupUI(template, fisherMachine);
                     createBatterySlot().setupUI(template, fisherMachine);
+                    createJunkButton().setupUI(template, fisherMachine);
                 }
             }));
 
@@ -414,6 +431,20 @@ public class FisherMachine extends TieredEnergyMachine
             slotWidget.setCanPutItems(true);
             slotWidget.setCanTakeItems(true);
             slotWidget.setHoverTooltips(LangHandler.getMultiLang("gtceu.gui.charger_slot.tooltip",
+                    GTValues.VNF[machine.getTier()], GTValues.VNF[machine.getTier()]).toArray(new MutableComponent[0]));
+        });
+    }
+
+    protected static EditableUI<ToggleButtonWidget, FisherMachine> createJunkButton() {
+        return new EditableUI<>("junk_button", ToggleButtonWidget.class, () -> {
+            var toggleButtonWidget = new ToggleButtonWidget(10, 20, 18, 18,
+                    new ItemStackTexture(Items.NAME_TAG).scale(0.9F), () -> false, b -> {});
+            toggleButtonWidget.setShouldUseBaseBackground();
+            return toggleButtonWidget;
+        }, (toggleButtonWidget, machine) -> {
+            toggleButtonWidget.setSupplier(machine::isJunkEnabled);
+            toggleButtonWidget.setOnPressCallback((data, bool) -> machine.setJunkEnabled(bool));
+            toggleButtonWidget.setHoverTooltips(LangHandler.getMultiLang("gtceu.gui.fisher_mode.tooltip",
                     GTValues.VNF[machine.getTier()], GTValues.VNF[machine.getTier()]).toArray(new MutableComponent[0]));
         });
     }
@@ -442,6 +473,10 @@ public class FisherMachine extends TieredEnergyMachine
             baitSlotWidget.setBackground(GuiTextures.SLOT, GuiTextures.STRING_SLOT_OVERLAY);
             baitSlotWidget.setId("bait_slot");
             main.addWidget(baitSlotWidget);
+            var junkButton = createJunkButton().createDefault();
+            junkButton.setSelfPosition(new Position(4, (main.getSize().height - junkButton.getSize().height) - 4));
+            junkButton.setId("junk_button");
+            main.addWidget(junkButton);
             main.setBackground(GuiTextures.BACKGROUND_INVERSE);
             return main;
         }, (group, machine) -> {
