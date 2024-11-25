be459c10cdba3c043e61626aa96a141d52e6bf33
kross <135918757+krossgg@users.noreply.github.com>
2024-11-25 09:39:28 +0200

    Fix Tank widgets (#2409)

--- src/main/java/com/gregtechceu/gtceu/integration/emi/recipe/GTEmiRecipe.java
+++ src/main/java/com/gregtechceu/gtceu/integration/emi/recipe/GTEmiRecipe.java
@@ -4,13 +4,28 @@ import com.gregtechceu.gtceu.api.recipe.GTRecipe;
 import com.gregtechceu.gtceu.integration.GTRecipeWidget;
 
 import com.lowdragmc.lowdraglib.emi.ModularEmiRecipe;
+import com.lowdragmc.lowdraglib.emi.ModularForegroundRenderWidget;
+import com.lowdragmc.lowdraglib.emi.ModularWrapperWidget;
+import com.lowdragmc.lowdraglib.gui.ingredient.IRecipeIngredientSlot;
+import com.lowdragmc.lowdraglib.gui.widget.DraggableScrollableWidgetGroup;
 import com.lowdragmc.lowdraglib.gui.widget.WidgetGroup;
+import com.lowdragmc.lowdraglib.jei.IngredientIO;
+import com.lowdragmc.lowdraglib.jei.ModularWrapper;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.RecipeHolder;
 
 import dev.emi.emi.api.recipe.EmiRecipeCategory;
+import dev.emi.emi.api.stack.EmiIngredient;
+import dev.emi.emi.api.widget.SlotWidget;
+import dev.emi.emi.api.widget.TankWidget;
+import dev.emi.emi.api.widget.Widget;
+import dev.emi.emi.api.widget.WidgetHolder;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GTEmiRecipe extends ModularEmiRecipe<WidgetGroup> {
 
